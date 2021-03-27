39175e84c62bf5cb74cff5bc6d22653ae13e533f
zarik5 <riccardo.zaglia5@gmail.com>
2021-03-27 11:06:50 +0100

    Populate project Add word count demo and homework 1 file

---
+++ src/main/java/G23HW1.java
@@ -0,0 +1,28 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class G23HW1 {
+    public static void main(String[] args) throws IOException {
+
+        // STEP 1
+
+
+        // STEP 2
+
+
+        // STEP 3
+
+
+        // STEP 4
+
+    }
+}
